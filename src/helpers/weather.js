71f1030e3189fa70bf91d42c43358db513836414
lefam <leonelmachava@gmail.com>
2018-11-16 20:20:43 +0200

    Add WeatherService

---
+++ src/helpers/weather.js
@@ -0,0 +1,18 @@
+import Api from "./api";
+
+export default class WeatherService {
+
+    /**
+     * Returns a promise which resolves to the current user's location.
+     * It may reject if the user rejects or another error occurs in the process.
+     *
+     * @returns {Promise<any>}
+     */
+    static getLocationConditions(lat, lng) {
+        const yql = `select item.condition from weather.forecast where woeid in (SELECT woeid FROM geo.places WHERE 
+            text="(${lat}, ${lng})")`;
+        const url = `https://query.yahooapis.com/v1/public/yql?q=${encodeURIComponent(yql)}`;
+
+        return Api.get(url);
+    }
+}
